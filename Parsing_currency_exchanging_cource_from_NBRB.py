647565b085ffe9b653723b346a9731f71cbfa22d
new <75909278+Alexander-050289@users.noreply.github.com>
2021-03-20 01:26:40 +0300

    Added a function@

--- Parsing_currency_exchanging_cource_from_NBRB.py
+++ Parsing_currency_exchanging_cource_from_NBRB.py
@@ -1,11 +1,26 @@
 import requests
 import json
+from contracts import contract
 
-currency = input("Name of currencies (ISO_4217)\n")
+currency = input("Name of currencies (ISO_4217)\n").upper()
 url = "https://www.nbrb.by/api/exrates/rates/" + currency + "?parammode=2"
 
-response = requests.get(url)
+def get_currency(some_currency, some_url):
+    """"
+    :param some_currency: currency you need to know
+    :type some_currency: str
+    :return: currency in string format
+    :rtype: dict
+    """
+    response = requests.get(some_url)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        print(data.get("Cur_Scale"), data.get("Cur_Abbreviation"), "-", data.get("Cur_OfficialRate"), "BYN at",
+          data.get("Date"))
+    else:
+        print("Input correct data or data not found")
 
-data = json.loads(response.text)
-print(data.get("Cur_Scale"), data.get("Cur_Abbreviation"), "-", data.get("Cur_OfficialRate"), "BYN at", data.get("Date"))
+if __name__ == "__main__":
+    get_currency(currency, url)
+
 
