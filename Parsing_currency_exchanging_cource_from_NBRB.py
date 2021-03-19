6a742c4937360071229fb3e7d2f5c2adba284a74
new <75909278+Alexander-050289@users.noreply.github.com>
2021-03-20 01:44:17 +0300

    Fixed some mistakes

--- Parsing_currency_exchanging_cource_from_NBRB.py
+++ Parsing_currency_exchanging_cource_from_NBRB.py
@@ -1,26 +1,30 @@
+# This code parsing site nbrb.by and get BYN exchange rates
+
 import requests
 import json
-from contracts import contract
 
-currency = input("Name of currencies (ISO_4217)\n").upper()
+
+currency = input("ISO_4217 currency name:\n").upper()
 url = "https://www.nbrb.by/api/exrates/rates/" + currency + "?parammode=2"
 
-def get_currency(some_currency, some_url):
+
+def get_currency(some_url):
+
     """"
-    :param some_currency: currency you need to know
-    :type some_currency: str
+    :param some_url: url according to which make get request
+    :type some_url: str
     :return: currency in string format
-    :rtype: dict
+    :rtype: str
     """
+
     response = requests.get(some_url)
     if response.status_code == 200:
         data = json.loads(response.text)
         print(data.get("Cur_Scale"), data.get("Cur_Abbreviation"), "-", data.get("Cur_OfficialRate"), "BYN at",
-          data.get("Date"))
+              data.get("Date"))
     else:
         print("Input correct data or data not found")
 
+
 if __name__ == "__main__":
-    get_currency(currency, url)
-
-
+    get_currency(url)
